a0c5a6a77705340ae09ad616dc17fc6db33f6df1
6cit-cheman <cheman@6cit.com>
2019-07-05 18:55:38 +0800

    add vuetify2.0 demo

---
+++ vuetify2.0/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-progress-linear :indeterminate="indeterminate" :style="line_style" color="green"></v-progress-linear>
+    <v-content>
+      <Home />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Home from "./components/Home";
+import { setTimeout } from "timers";
+
+export default {
+  name: "App",
+  components: {
+    Home
+  },
+  data: () => ({
+    //
+    indeterminate: true,
+    line_style: {
+      display: ""
+    }
+  }),
+  created() {
+    setTimeout(() => {
+      this.indeterminate = false;
+      this.line_style.display = "none";
+    }, 3000);
+  }
+};
+</script>
---
+++ vuetify2.0/src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-app-bar
+      color="transparent"
+      :hide-on-scroll="true"
+      flat
+      :elevate-on-scroll="true"
+      height="120px"
+      src="https://picsum.photos/1920/1080?random"
+    >
+      <template v-slot:img="{ props }">
+        <v-img v-bind="props" gradient="to top right, rgba(0,0,0,.0), rgba(0,0,0,.0)"></v-img>
+      </template>
+      <v-toolbar-items>
+        <v-btn text>HOME</v-btn>
+        <v-btn text>DISPLAY</v-btn>
+        <v-btn text>INFO</v-btn>
+        <v-btn text>ME</v-btn>
+        <v-btn text>SKILL</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({})
+};
+</script>
+<style>
+</style>
+
